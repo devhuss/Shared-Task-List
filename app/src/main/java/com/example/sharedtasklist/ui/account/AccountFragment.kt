f09130d805489b27a981190ee7440041fd66def4
JMDoza <Mendozaji327@gmail.com>
2022-04-28 17:12:40 -0400

    Tested pushing data into the database in account tab

--- app/src/main/java/com/example/sharedtasklist/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/sharedtasklist/ui/account/AccountFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
@@ -13,9 +15,12 @@ import com.example.sharedtasklist.R
 import com.example.sharedtasklist.databinding.AccountFragmentBinding
 import com.example.sharedtasklist.databinding.FragmentNotificationsBinding
 import com.example.sharedtasklist.ui.notifications.NotificationsViewModel
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class AccountFragment : Fragment() {
 
@@ -50,6 +55,11 @@ class AccountFragment : Fragment() {
         val userName: TextView = binding.tUName
         val userEmail: TextView = binding.tUEmail
         val userImg: ImageView = binding.imgUser
+        val btnPost: Button = binding.bPostStatus
+
+        btnPost.setOnClickListener {
+            postStatus()
+        }
 
         userName.text = currentUser?.displayName.toString()
         userEmail.text = currentUser?.email.toString()
@@ -62,7 +72,27 @@ class AccountFragment : Fragment() {
         accountViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
         return root
+    }
+
+    private fun postStatus () {
+        var db = FirebaseFirestore.getInstance()
+        val postsRef = db.collection("posts")
+
+        val etStatus: EditText = binding.etPostStatus
+
+
+
+        val statusInfo = hashMapOf(
+            "createdAt" to Timestamp(Date()),
+            "createdBy" to auth.currentUser?.uid,
+            "Status" to etStatus.text.toString(),
+        )
+
+        postsRef.add(statusInfo)
+
     }
 
 //    override fun onActivityCreated(savedInstanceState: Bundle?) {
