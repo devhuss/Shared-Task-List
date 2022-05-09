c30056076e3fc8d3fcec678a07b6df6d15f00caf
JMDoza <Mendozaji327@gmail.com>
2022-05-09 05:39:15 -0400

    Removed unnecessary bottom tabs and their respective layouts

--- app/src/main/java/com/example/sharedtasklist/HomeActivity.kt
+++ app/src/main/java/com/example/sharedtasklist/HomeActivity.kt
@@ -37,7 +37,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_account
+                R.id.navigation_home, R.id.navigation_messages, R.id.navigation_account
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/sharedtasklist/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/sharedtasklist/ui/account/AccountFragment.kt
@@ -11,11 +11,10 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.sharedtasklist.R
 import com.example.sharedtasklist.databinding.AccountFragmentBinding
-import com.example.sharedtasklist.databinding.FragmentNotificationsBinding
-import com.example.sharedtasklist.ui.notifications.NotificationsViewModel
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
